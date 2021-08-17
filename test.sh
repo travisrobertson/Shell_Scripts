984700f7ec0efa5908f37d60b7a8d73e68c34b29
travisrobertson <trobertson@tia.toppan.com>
2021-08-17 12:17:31 -0400

    updated test

--- test.sh
+++ test.sh
@@ -17,9 +17,15 @@ while [ $loop_count -lt $number_of_files ]
         echo "base_filename = $base_filename"
         echo "loop number = $loop_count"
         work_file=$base_filename.$loop_count
+        modified_date=$(stat -c %y $work_file)
         echo "work file = $work_file"
         file_modified_date=$(date -r $work_file "+%Y%m%d")
         echo "file modified date = $file_modified_date"
+        commit_message="$base_filename was modified on $file_modified_date"
+        echo $commit_message
+        echo "git add file.name"
+        echo "GIT_AUTHOR_DATE=$modified_date GIT_COMMITTER_DATE=$modified_date git commit -m quoted message"
+        echo ""
         echo ""
         sleep 1
     done
